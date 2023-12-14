df68bfd6811070ab2fb6a15b5683d079cedae003
its-s23028 <s23028@std.it-college.ac.jp>
2023-12-14 06:51:32 +0000

    p57
    カスタムAppコンポーメントでLayoutを全ページに入れた

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Layout from 'components/layout'
 import Hero from 'components/hero'
 
 const Home = () => {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  )
+  return <Hero />
 }
 
 export default Home
