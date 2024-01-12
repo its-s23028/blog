2c23f6f89d1176bd9e24bd91e82f96df785f9970
its-s23028 <s23028@std.it-college.ac.jp>
2024-01-12 06:00:10 +0000

    251p
    アイキャッチ画像eyecatchの大換画像とブラー画像を用意する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,8 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { eyecatchLocal } from 'lib/constants'
+import { getPlaiceholder } from 'plaiceholder'
 
 export default function Schedule ({
   title,
@@ -43,6 +45,8 @@ export default function Schedule ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
         <TwoColumn>
@@ -61,18 +65,23 @@ export default function Schedule ({
 }
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description
     }
